30f745db6a86a4a434b57dbcd73c2b097795382d
CainaPV <cainaverona@gmail.com>
2024-03-20 19:36:08 -0300

    config allowed_hosts

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gu7@01idf^!al&j5eda5hv^zme-&ir)_k0_ub6ky0utppvdx)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
