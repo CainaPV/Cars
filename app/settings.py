f576eb515c0d311e21e1bca3b5021e2e910886ba
CainaPV <cainaverona@gmail.com>
2024-03-28 08:54:14 -0300

    /Settings.py DEBUG False FOR TRUE

--- app/settings.py
+++ app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gu7@01idf^!al&j5eda5hv^zme-&ir)_k0_ub6ky0utppvdx)3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
